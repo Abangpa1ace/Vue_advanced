a2176cf0ada9e67c98472e7c29a5851965a86368
Taeng <dyek72731@gmail.com>
2021-06-16 01:09:13 +0900

    Vue 미니프로젝트 수정중

--- lv3-hacker-news/src/views/AskView.vue
+++ lv3-hacker-news/src/views/AskView.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
-    ask
+    <div 
+      v-for="ask in askList"
+      :key="ask.title">
+      {{ ask.title }}
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import { fetchAskList } from '../api';
+
+export default {
+  data() {
+    return {
+      askList: [],
+    }
+  } ,
+  created() {
+    fetchAskList()
+      .then(res => this.askList = res.data)
+      .catch(err => console.log(err))
+      .then(console.log(askList))
+  },
+}
 </script>
 
 <style scoped>
--- lv3-hacker-news/src/views/JobsView.vue
+++ lv3-hacker-news/src/views/JobsView.vue
@@ -5,9 +5,20 @@
 </template>
 
 <script>
-  export default {
-    
-  }
+import { fetchJobsList } from '../api';
+
+export default {
+  data() {
+    return {
+      jobsList: [],
+    }
+  } ,
+  created() {
+    fetchJobsList()
+      .then(res => this.jobsList = res.data)
+      .catch(err => console.log(err))
+  },
+}
 </script>
 
 <style scoped>
